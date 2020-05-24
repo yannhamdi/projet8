3e2d8718d84d27d4275603e90bd4c61396db6a27
YANN HAMDI <hamdiyann@gmail.com>
2020-05-24 16:00:07 +0100

    ajout api openfood

---
+++ p8/products/import_api.py
@@ -0,0 +1,20 @@
+""" module that import products from openfood api"""
+
+import requests
+
+
+def get_json():
+    """method that look for products in the openfoodfact api"""
+    url = 'https://fr.openfoodfacts.org/cgi/search.pl'
+    params = {
+               "action": "process",
+               "json": 1,
+               "page_size": 1,
+             }
+    response = requests.get(url, params=params)
+    print(response)
+    products = []
+    if response.status_code == 200:
+        products = response.json()['products']
+
+    return products
