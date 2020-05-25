73b2c8753bc681b47bdda31c5ca5b577648a921d
YANN HAMDI <hamdiyann@gmail.com>
2020-05-25 20:49:45 +0100

    modif modeles

--- products/management/commands/dbopenfood.py
+++ products/management/commands/dbopenfood.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
-from products.models import Category, Cat_prod, Products
+from products.models import Category, Products
 from products.import_api import get_json
 
 
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         """ funcion that will call all the methods to fill our database"""
         self.prod = get_json()
         self.saving_cat()
-
+        self.create_product()
     def saving_cat(self):
         """methods that fills the cat model"""
         popo = self.prod
@@ -32,11 +32,11 @@ class Command(BaseCommand):
         """methods that fills up our model product"""
         db_pro = self.prod
         i = 0
-        while i <= (len(products)-1):
-            Products.objects.get_or_create(id_code=products[i]["code"],
-                                    food_name=products[i]["product_name"],
-                           nutrition_grade=products[i]["nutrition_grade_fr"],
-                                                 food_link=products[i]["url"],
-                                          image_url=products[i]["image_url"])
+        while i <= (len(db_pro)-1):
+            Products.objects.get_or_create(id_code=db_pro[i]["code"],
+                                    food_name=db_pro[i]["product_name"],
+                           nutrition_grade=db_pro[i]["nutrition_grade_fr"],
+                                                 food_link=db_pro[i]["url"],
+                                          image_url=db_pro[i]["image_url"])
             i = i+1
         
