dfa94e75d2ad344394a49694e05bf07bee028ef5
YANN HAMDI <hamdiyann@gmail.com>
2020-06-05 12:05:20 +0100

    test et signup

--- users/views.py
+++ users/views.py
@@ -5,10 +5,9 @@ from .forms import SignUpForm
 
 def signup(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST or None)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            auth.login(request, user)
-            return redirect('search')
+            auth_login(request, user)
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
