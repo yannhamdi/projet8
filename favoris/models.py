1da5289a8f401f34f9e01457fd149a5a7987fcdc
YANN HAMDI <hamdiyann@gmail.com>
2020-06-06 10:21:46 +0100

    creation model favoris

--- favoris/models.py
+++ favoris/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from users.models import User 
+from products.models import Products
+
+
+class Favorite(models.Model):
+    """class that create a model favoris that will connect the user's model and the products' model"""
+    user_link = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user_favoris")
+    product_searched = models.ForeignKey("products.Products", on_delete=models.CASCADE, related_name="user_search")
+    product_substitute = models.ForeignKey("products.Products", on_delete=models.CASCADE, related_name="product_substitute")
+
+    def __str__(self):
+        return self.product_substitute.food_name
