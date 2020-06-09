bcbf1996e24871b01328932e7c9c0fef060d1e76
YANN HAMDI <hamdiyann@gmail.com>
2020-06-09 19:38:38 +0100

    ajout mock api openfood

--- products/tests/test_import_api.py
+++ products/tests/test_import_api.py
@@ -1,43 +1,25 @@
 
-import unittest
-from unittest import mock
+from django.test import TestCase
+from unittest.mock import patch, MagicMock
 from products.import_api import get_json
-import json
 import requests
 
-# This method will be used by the mock to replace requests.get
-def mocked_requests_get(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
-
-        def json(self):
-            return self.json_data
-
-    if args[0] == 'http://someurl.com/test.json':
-        return MockResponse({"key1": "value1"}, 200)
-    elif args[0] == 'http://someotherurl.com/anothertest.json':
-        return MockResponse({"key2": "value2"}, 200)
-
-    return MockResponse(None, 404)
+class TestImportApi(TestCase):
+    @patch('requests.get')
+    def test_get_json(self,mockget_json):
+        category = "pizzas"
+        mockget_json.return_value.json.return_value ={
+        "products": [
+          { "code": 122233,
+                "product_name": "pizzas",
+                "nutrition_grade_fr": "a",
+                "url": "www.dkdjkdjd.com",
+                "image_url": "www.image.fr"
 
 
-class MyGreatClassTestCase(unittest.TestCase):
-
-    # Now we must patch 'my.great.package.requests.get'
-    @mock.patch('requests.get', side_effect=mocked_requests_get)
-    def test_fetch(self, mock_get):
-        getj = get_json("category")
-        json_data = getj.fetch_json('http://someurl.com/test.json')
-        self.assertEqual(json_data, {"key1": "value1"})
-        json_data = getj.fetch_json('http://someotherurl.com/anothertest.json')
-        self.assertEqual(json_data, {"key2": "value2"})
-        json_data = getj.fetch_json('http://nonexistenturl.com/cantfindme.json')
-        self.assertIsNone(json_data)
-
-        # We can even assert that our mocked method was called with the right parameters
-        self.assertIn(mock.call('http://someurl.com/test.json'), mock_get.call_args_list)
-        self.assertIn(mock.call('http://someotherurl.com/anothertest.json'), mock_get.call_args_list)
-
-        self.assertEqual(len(mock_get.call_args_list), 3)
+              }]
+              }
+        mockget_json.return_value.status_code = 200
+        prod = get_json(category)
+        self.assertEqual(len(prod),1)
+          
