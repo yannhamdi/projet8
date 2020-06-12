fdae05497b3b64eb78c05fa372d690cb4ecc57ff
YANN HAMDI <hamdiyann@gmail.com>
2020-06-12 14:55:18 +0100

    test validationerror

--- users/tests/test_forms_signup.py
+++ users/tests/test_forms_signup.py
@@ -17,6 +17,16 @@ class SignUpFormTest(TestCase):
         expected = ['username', 'password']
         actual = list(form.fields)
         self.assertSequenceEqual(expected,actual)
+    def test_signup_valid(self):
+        data = {"model": User}
+        form = SignUpForm(data)
+        self.assertFalse(form.is_valid())
+    def test_signin_valid(self):
+        user, created = User.objects.get_or_create(username="papa", last_name="sebti", first_name="zahia", email="hh@hotmail.com", password="123456789")
+        data = {"username": user.username, "password": user.password}
+        form = SignInForm(data)
+        self.assertTrue(form.is_valid())
+    
     def test_clean_fake_password(self):
         test = SignInForm()
         user, created = User.objects.get_or_create(username="papa", last_name="sebti", first_name="zahia", email="hh@hotmail.com", password="123456789")
@@ -31,4 +41,3 @@ class SignUpFormTest(TestCase):
         password_entry = "123456789"
         user = auth.authenticate(username=username_entry, password=password_entry)
         self.assertRaises(ValidationError)
-        
