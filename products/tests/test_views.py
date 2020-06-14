0d0aa2e898e3be227eef8529375a6010483f937f
YANN HAMDI <hamdiyann@gmail.com>
2020-06-14 19:55:58 +0100

    test vues products

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-
+from products.forms import ProductSearch
 from products.views import display, lire, search
 from products.models import Category, Products
 from random import randint
@@ -22,6 +22,9 @@ class ProductsView(TestCase):
     def test_search(self):
         response = self.client.get(reverse('search'))
         self.assertEqual(response.status_code, 200)
+    def test_search_with_post(self):
+        response_1 = self.client.post(reverse('search'), {'search': 'reine'} )
+        self.assertEqual(response_1.status_code, 302)
 
     def test_lire(self):
         response = self.client.post(reverse('lire', args=[20930395]))
@@ -30,23 +33,7 @@ class ProductsView(TestCase):
     def test_lire_error(self):
         response = self.client.post(reverse('lire', args=[2093]))
         self.assertEqual(response.status_code, 404)
-    def test_product_search(self):
-        produ_id = Products.objects.filter(food_name__icontains="reine")
-        category_product = produ_id[0].category.all()
-        cat = category_product[0]
-        better_product = Products.objects.filter(category=cat).filter(nutrition_grade__lt=produ_id[0].nutrition_grade)
-        i=(len(better_product))-1
-        j=randint(0,i)
-        cat = category_product[0]
-        better_product = Products.objects.filter(category=cat).filter(nutrition_grade__lt=produ_id[0].nutrition_grade)
-        better = better_product[0].food_name
-        prod_2 = self.product_2.food_name
-        prod_1= self.product_1.food_name
-        prod_id = produ_id[0].food_name
-        response =self.client.get(reverse('display', args=[int(produ_id[0].id_code), int(better_product[j].id_code)]))
-        self.assertEqual(prod_1, prod_id)
-        self.assertEqual(prod_2, better)
-        self.assertEqual(response.status_code, 200)
+    
 
     
     def test_display(self):
