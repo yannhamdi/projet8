266ce23709d5681fc3ab668d988f74abe65a9967
YANN HAMDI <hamdiyann@gmail.com>
2020-06-17 18:31:24 +0100

    correction bug affichage favoris

--- products/views.py
+++ products/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.urls import reverse
-from random import randint
 from products.models import Products
 
 from .forms import ProductSearch
