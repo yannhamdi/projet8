57512dca81eb232fce14fc9d84b25bfa3927b2d2
YANN HAMDI <hamdiyann@gmail.com>
2020-06-17 23:03:08 +0100

    essai creative bootstrap

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, reverse
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="products/home.html"), name='home'),
     path('article/<int:id>', views.lire, name='lire'),
     path('search/', views.search, name='search'),
     path('display/<int:id_searched>/<int:id_substitue>', views.display, name='display')
