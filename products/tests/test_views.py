e49da7e68534111d0d6d9eec041b87fde891d4ee
YANN HAMDI <hamdiyann@gmail.com>
2020-06-22 20:44:57 +0100

    ajout test integration

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -9,12 +9,12 @@ class ProductsView(TestCase):
     def setUp(self):
         self.category = Category.objects.get_or_create(cat= "pizzas")
         self.product_1, created = Products.objects.get_or_create(id_code=20930393, food_name="reine", nutrition_grade="c",
-                                             image_url="www.deoeoei.fr", food_link="www.reine.fr")
+                                image_url="www.deoeoei.fr", food_link="www.reine.fr", image_nutrition_url="www.oeoeouou.fr")
         s = Category.objects.get(cat="pizzas")
         self.product_1.category.add(s)
         self.product_1.save()
         self.product_2, created = Products.objects.get_or_create(id_code=20930395, food_name="marguerite", nutrition_grade="a",
-                                             image_url="www.deoeoli.fr", food_link="www.marguerite.fr")
+                                image_url="www.deoeoli.fr", food_link="www.marguerite.fr", image_nutrition_url="www.oeoeouou.fr")
         self.product_2.category.add(s)
         self.product_2.save()
 
@@ -27,9 +27,6 @@ class ProductsView(TestCase):
     def test_search_with_post(self):
         response_1 = self.client.post(reverse('search'), {'search': 'reine'} )
         self.assertEqual(response_1.status_code, 302)
-    def test_search_no_result(self):
-        response = self.client.get(reverse('no_response'))
-        self.assertEqual(response.status_code, 200)
     def test_lire(self):
         response = self.client.post(reverse('lire', args=[20930395]))
         self.assertEqual(response.status_code, 200)
--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('article/<int:id>', views.lire, name='lire'),
     path('search/', views.search, name='search'),
-    path('display/<int:id_searched>/<int:id_substitue>', views.display, name='display')
+    path('display/<int:id_searched>/<int:id_substitue>', views.display, name='display'),
+   
 
 
 ]
