fe164976eb54a847b1486ea25f3c42fb82cf4fba
YANN HAMDI <hamdiyann@gmail.com>
2020-06-22 22:34:25 +0100

    pep8

--- products/forms.py
+++ products/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class ProductSearch(forms.Form):
-    search = forms.CharField(label="",max_length=100,
-    	     widget=forms.TextInput(attrs={'placeholder':"Entrez votre recherche"}))
+    search = forms.CharField(label="", max_length=100,
+                             widget=forms.TextInput(attrs={'placeholder':
+                                                    "Entrez votre recherche"}))
