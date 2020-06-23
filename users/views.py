e5b0d28744a256f09717b300dc46cbcbdf333904
YANN HAMDI <hamdiyann@gmail.com>
2020-06-23 19:56:00 +0100

    pep8

--- users/views.py
+++ users/views.py
@@ -3,12 +3,11 @@ from django.contrib.auth import authenticate, logout
 from django.core.exceptions import ValidationError
 from django.contrib import auth
 from django.shortcuts import render, redirect
-from django.conf import settings
-from django import forms
 
 from .forms import SignUpForm, SignInForm
 from users.models import User
 from products.forms import ProductSearch
+
 
 def signup(request):
     """views that allows the user to signup"""
@@ -23,11 +22,9 @@ def signup(request):
     return render(request, 'registration/signup.html', {'form': form})
 
 
-
 def signin(request):
     """view that connect the user"""
     if request.method == 'POST':
-        
         form = SignInForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data["username"]
@@ -35,9 +32,6 @@ def signin(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                context = {
-                    "user": request.user,
-                }
                 nextt = request.GET.get('next', 'home')
                 if nextt:
                     return redirect(nextt)
@@ -45,16 +39,16 @@ def signin(request):
         form = SignInForm()
     return render(request, 'registration/signin.html', {"form": form})
 
+
 def signout(request):
     """views that logs out"""
     auth.logout(request)
     return redirect('home')
 
+
 def account(request):
     """ views that display user's details"""
     user_details = User(request)
     form = ProductSearch(request.POST or None)
-    return render(request, 'registration/account.html', {'user_details': user_details, 'form': form})
-
-
-
+    return render(request, 'registration/account.html',
+                  {'user_details': user_details, 'form': form})
