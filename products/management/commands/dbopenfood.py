686917699392c22fbbcdb02e1115ad4d63ccdbe6
YANN HAMDI <hamdiyann@gmail.com>
2020-06-23 19:44:16 +0100

    pep8

--- products/management/commands/dbopenfood.py
+++ products/management/commands/dbopenfood.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.conf import settings
 
 from products.models import Category, Products
@@ -9,7 +9,6 @@ class Command(BaseCommand):
     """personnalized command"""
     help = "commands to fill up our database"
 
-
     def handle(self, *args, **options):
         """ funcion that will call all the methods to fill our database"""
         self.delete_all()
@@ -17,30 +16,32 @@ class Command(BaseCommand):
             self.saving_category(category)
         for category in settings.FOOD_CATEGORIES:
             self.saving_cat(category)
+
     def checking_blank(self, element):
         """ method that check blank lines"""
-        keys = ["code", "product_name", "nutrition_grade_fr", "url", "image_url", "categories", "image_nutrition_url"]
+        keys = ["code", "product_name", "nutrition_grade_fr", "url",
+                "image_url", "categories", "image_nutrition_url"]
         for key in keys:
             if key not in element or not element[key]:
                 return False
         return True
 
     def saving_category(self, category):
-        Category.objects.get_or_create(cat=category)   
+        Category.objects.get_or_create(cat=category)
 
     def saving_cat(self, category):
         """methods that fills the cat model"""
         self.category = category
-        
         self.db_product = get_json(category)
         for element in self.db_product:
             if self.checking_blank(element):
-                product, created =Products.objects.get_or_create(id_code=element["code"],
+                product, created = Products.objects.get_or_create(
+                        id_code=element["code"],
                         food_name=element["product_name"],
                         nutrition_grade=element["nutrition_grade_fr"],
-                                    food_link=element["url"],
-                             image_url=element["image_url"],
-                             image_nutrition_url =element["image_nutrition_url"])
+                        food_link=element["url"],
+                        image_url=element["image_url"],
+                        image_nutrition_url=element["image_nutrition_url"])
                 papa = (element["categories"]).split(",")
                 for item in papa:
                     p = (item.lower()).strip()
@@ -48,6 +49,7 @@ class Command(BaseCommand):
                         s = Category.objects.get(cat=p)
                         product.category.add(s)
                         product.save()
+
     def delete_all(self):
         Products.objects.all().delete()
         Category.objects.all().delete()
