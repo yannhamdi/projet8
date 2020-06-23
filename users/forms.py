d2d5de92156cd83e254838a8034d1a2b9dfd8d9c
YANN HAMDI <hamdiyann@gmail.com>
2020-06-23 19:50:26 +0100

    pep8

--- users/forms.py
+++ users/forms.py
@@ -1,20 +1,24 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate
-from django.contrib import auth
 from django.core.exceptions import ValidationError
 from .models import User
 
+
 class SignUpForm(UserCreationForm):
+
     class Meta:
         model = User
-        fields = ('username', 'last_name', 'first_name', 'email', 'password1', 'password2')
+        fields = ('username', 'last_name', 'first_name',
+                  'email', 'password1', 'password2')
 
 
 class SignInForm(forms.Form):
-    username = forms.CharField(label= "username", max_length=20)
-    password = forms.CharField(label = "Mot-de-Passe", widget=forms.PasswordInput, min_length = 6, max_length= 16)
+    username = forms.CharField(label="username", max_length=20)
+    password = forms.CharField(label="Mot-de-Passe",
+                               widget=forms.PasswordInput,
+                               min_length=6, max_length=16)
+
     def clean(self):
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
@@ -24,6 +28,4 @@ class SignInForm(forms.Form):
                                            password=password)
             if self.user_cache is None:
                 raise forms.ValidationError('Identifiants incorrects')
-                   
-
         return self.cleaned_data
