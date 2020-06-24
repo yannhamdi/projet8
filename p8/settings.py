7a66588a23f76e1631b734ee376a3afa29241f3b
YANN HAMDI <hamdiyann@gmail.com>
2020-06-24 18:22:07 +0100

    static

--- p8/settings.py
+++ p8/settings.py
@@ -161,3 +161,14 @@ AUTH_USER_MODEL = "users.User"
 
 LOGIN_URL = "/users/signin"
 LOGOUT_REDIRECT_URL = "/products/home"
+if os.environ.get('ENV') == 'PRODUCTION':
+
+    # Static files settings
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
