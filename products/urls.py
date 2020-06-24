0fac5f61ad49d42b12a3501f13eca43a7728a497
YANN HAMDI <hamdiyann@gmail.com>
2020-06-24 10:56:48 +0100

    mentions legales

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('article/<int:id>', views.lire, name='lire'),
     path('search/', views.search, name='search'),
     path('display/<int:id_searched>/<int:id_substitue>', views.display, name='display'),
+    path('mentions_legales', views.mention, name="mention"),
    
 
 
