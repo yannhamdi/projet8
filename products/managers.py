294f8ee51dd8ca6b79742b0b7462281c313d9daa
YANN HAMDI <hamdiyann@gmail.com>
2020-06-24 01:50:08 +0100

    correction bug managers si pas produit de substitution

--- products/managers.py
+++ products/managers.py
@@ -9,21 +9,28 @@ class ProductManager(models.Manager):
         returning_request_user = {}
         produ_id = None
         produ_id = self.filter(food_name__icontains=search_entry)
-        if produ_id:
-            category_product = produ_id[0].category.all()
+        category_product = produ_id[0].category.all()
+        if produ_id and category_product:
             if len(category_product) > 1:
                 cat = category_product[0]
                 better_product = self.filter(category=cat).filter(
                     nutrition_grade__lt=produ_id[0].nutrition_grade)
-                i = (len(better_product))-1
-                j = randint(0, i)
+                if better_product:
+                    i = (len(better_product))-1
+                    j = randint(0, i)
+                    returning_request_user = {'product_searched':
+                                      produ_id[0].id_code, 'substitue':
+                                      better_product[j].id_code}
+                return None
             cat = category_product[0]
             better_product = self.filter(category=cat).filter(
                 nutrition_grade__lt=produ_id[0].nutrition_grade)
-            i = (len(better_product))-1
-            j = randint(0, i)
-            returning_request_user = {'product_searched':
+            if better_product:
+                i = (len(better_product))-1
+                j = randint(0, i)
+                returning_request_user = {'product_searched':
                                       produ_id[0].id_code, 'substitue':
                                       better_product[j].id_code}
-            return returning_request_user
+                return returning_request_user
+            return None
         return None
