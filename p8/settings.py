fc4c0f9fe76753fff0acd34e8079830bbec65508
YANN HAMDI <hamdiyann@gmail.com>
2020-06-24 20:21:01 +0100

    settings

--- p8/settings.py
+++ p8/settings.py
@@ -32,7 +32,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['p8-yh.herokuapp.com']
+ALLOWED_HOSTS = ['p8-yh.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
--- p8/urls.py
+++ p8/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from products.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('users/', include('users.urls')),
     path('favoris/', include('favoris.urls')),
+    path('', home, name="home"),
+  
 ]
