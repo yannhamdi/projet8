3f5e4c1d51a753d6780241e94c077a606baeddb1
YANN HAMDI <hamdiyann@gmail.com>
2020-11-01 13:50:33 +0100

    efface heroku du setting

--- p8/settings/__init__.py
+++ p8/settings/__init__.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
 import os
 import dj_database_url
 # ...
