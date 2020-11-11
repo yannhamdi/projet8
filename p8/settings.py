f757361a5572ffc8cd56db1bcdc0ab843fac7761
YANN HAMDI <hamdiyann@gmail.com>
2020-11-11 09:49:43 +0100

    settigns

--- p8/settings.py
+++ p8/settings.py
@@ -135,8 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_ROOT, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static")),
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 FOOD_CATEGORIES = [
     "fromages",
