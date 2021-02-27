9341817acd4144cc1cb4eeb124707871d81d6376
YANN HAMDI <hamdiyann@gmail.com>
2021-02-27 19:23:58 +0100

    settings

--- p8/settings.py
+++ p8/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'nutella',
         'USER': 'yannhamdi',
         'PASSWORD': 'yh250980',
-        'HOST': '',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
