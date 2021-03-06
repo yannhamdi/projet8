6278e20375d8a19dc11684702e6e04b247886b38
YANN HAMDI <hamdiyann@gmail.com>
2021-03-06 11:38:37 +0100

    template registration

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ path('products/', include('products.urls')),
 path('signin/', views.signin, name="signin"),
  path('account/', views.account, name="account"),
 path("signout/", views.signout, name="signout"),
+path("change_password/", views.change_password, name="change_password"),
 ]
--- users/views.py
+++ users/views.py
@@ -52,3 +52,19 @@ def account(request):
     form = ProductSearch(request.POST or None)
     return render(request, 'registration/account.html',
                   {'user_details': user_details, 'form': form})
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {
+        'form': form
+    })
