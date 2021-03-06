1b0bad556ef9f046c3df7bb4d5b4bcbb151a7efb
YANN HAMDI <hamdiyann@gmail.com>
2021-03-06 12:27:31 +0100

    change password

--- users/views.py
+++ users/views.py
@@ -60,10 +60,7 @@ def change_password(request):
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
-            messages.success(request, 'Your password was successfully updated!')
-            return redirect('change_password')
-        else:
-            messages.error(request, 'Please correct the error below.')
+            return redirect('password_changed')
     else:
         form = PasswordChangeForm(request.user)
     return render(request, 'registration/change_password.html', {
