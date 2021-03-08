e388e35b39f62c45c5e9839dd72500e17f01a03e
YANN HAMDI <hamdiyann@gmail.com>
2021-03-08 14:51:02 +0100

    reset password

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,6 @@ path('signin/', views.signin, name="signin"),
  path('account/', views.account, name="account"),
 path("signout/", views.signout, name="signout"),
 path("change_password/", views.change_password, name="change_password"),
+path('users/', include('django.contrib.auth.urls')),
 
 ]
