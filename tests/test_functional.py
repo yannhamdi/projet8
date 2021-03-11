1bcb8a7c01640d88af79437d2e4c5919a7964a54
YANN HAMDI <hamdiyann@gmail.com>
2021-03-11 16:47:56 +0100

    selenium

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,26 +1,31 @@
-from django.urls import reverse
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import LiveServerTestCase
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import Firefox
 from selenium import webdriver
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.urls import reverse
 
-from users.models import User
-
+browser= webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
 firefox_options = webdriver.FirefoxOptions()
 firefox_options.headless = True
 
 
-class TestSelenium(StaticLiveServerTestCase):
+class CustomerTestCase(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.selenium = webdriver.Firefox(firefox_options=firefox_options)
-        cls.selenium.implicitly_wait(10)
-
+        cls.browser = Firefox()
+        cls.browser.implicitly_wait(50)
+    
     @classmethod
     def tearDownClass(cls):
+        cls.browser.quit()
         super().tearDownClass()
-        cls.selenium.quit()
+
 
     def setUp(self):
+        User = get_user_model()
         self.password = "affdLhj23HJ"
         self.user = User.objects.create_user(
             username="testuser",
@@ -29,14 +34,14 @@ class TestSelenium(StaticLiveServerTestCase):
         )
 
     def test_login(self):
-        self.selenium.get(self.live_server_url + reverse('signin'))
-        username_input = self.selenium.find_element_by_name("username")
+        self.browser.get(self.live_server_url + reverse('signin'))
+        username_input = self.browser.find_element_by_name("username")
         username_input.send_keys(self.user.username)
-        password_input = self.selenium.find_element_by_name("password")
+        password_input = self.browser.find_element_by_name("password")
         password_input.send_keys(self.password)
-        button = self.selenium.find_element_by_id("go")
+        button = self.browser.find_element_by_id("go")
         button.click()
-        logout_button = self.selenium.find_element_by_css_selector(
+        logout_button = self.browser.find_element_by_css_selector(
             f"a[href=\"{reverse('signout')}\"]"
         )
         self.assertTrue(logout_button.is_displayed)
