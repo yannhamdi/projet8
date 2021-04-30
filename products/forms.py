5c9a0eec83c34a4e8749db10951830e783f105f1
YANN HAMDI <hamdiyann@gmail.com>
2021-04-30 21:39:31 +0100

    geckodriver

--- products/forms.py
+++ products/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class ProductSearch(forms.Form):
     search = forms.CharField(label="", max_length=100,
                              widget=forms.TextInput(attrs={'placeholder':
-                                                    "Entrez votre recherche"}))
+                                                    "Entrez votre recherche"})) 
+                                                   
