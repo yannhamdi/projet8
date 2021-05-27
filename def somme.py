8408230d982e32522a83c4c5a8dc58a1e2361f32
YANN HAMDI <hamdiyann@gmail.com>
2021-05-27 17:24:17 +0100

    geckodriver

---
+++ def somme.py
@@ -0,0 +1,10 @@
+def somme():
+    s= o
+    for k in range(567):
+        s = s+k
+    print s
+
+
+def main():
+    somme()
+main()
---
+++ somme.py
@@ -0,0 +1,16 @@
+def somme():
+    s= 1536
+    z = 0
+    k = "6"
+    j= 0
+    for i in range(1537):
+    	str(i)
+    	if k in i:
+    		z = z+ 1
+    print(z)
+
+
+
+def main():
+    somme()
+main()
